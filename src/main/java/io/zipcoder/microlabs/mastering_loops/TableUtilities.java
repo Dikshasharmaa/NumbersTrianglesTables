40fee008c6c63f204d6a410c6810e5a2fdacd030
Dikshasharmaa <diksha66sharma78@gmail.com>
2024-03-05 23:21:02 -0500

    Finished

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -1,18 +1,44 @@
 package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
+    public static String formatNum(int num){
+        String num1 = String.valueOf(num);
+        int len = num1.length();
+        if(len == 1){
+            return "  "+num1+" ";
+        }
+        else if(len == 2){
+            return " "+num1+" ";
+        }
+        else if(len == 3){
+            return num1+" " ;
+        }
+        else{
+            return num1;
+        }
+
+
+    }
+
     public static String getSmallMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(10);
+
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        for (int i =0;i<tableSize;i++){
-            
+        String result ="";
+        for (int i =1;i<=tableSize;i++){
+            for(int j=1;j<=tableSize;j++){
+                result += formatNum(j*i)+"|";
+            }
+            result += "\n";
+
         }
-        return null;
+
+        return result;
     }
 }
